3dfe40e9f1b4cec4d1e63e43e72db039388ff1c2
y-nishizaki <125111163+y-nishizaki@users.noreply.github.com>
2023-04-16 21:27:24 +0900

    test_core.py を更新

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,24 +1,28 @@
+import os
 import pandas as pd
-from sklearn.datasets import fetch_openml
-from boostora.boostora import run_framework
+import unittest
+from boostora import Boostora
+
+class TestBoostora(unittest.TestCase):
+    def test_classification(self):
+        df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+        df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+        df['species'] = df['species'].map({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
+
+        target_col = 'species'
+
+        boostora = Boostora(classification=True)
+        boostora.run_framework(df, target_col, experiment_name="Boostora_Classification_Test", n_trials=10)
+
+    def test_regression(self):
+        df = pd.read_csv('https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.data', header=None, sep='\s+')
+        df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+        target_col = 'MEDV'
+
+        boostora = Boostora(classification=False)
+        boostora.run_framework(df, target_col, experiment_name="Boostora_Regression_Test", n_trials=10)
 
 
-def custom_param_tuning(trial):
-    param = {
-        "objective": "reg:squarederror",
-        "eval_metric": "rmse",
-        "booster": trial.suggest_categorical("booster", ["gbtree", "gblinear"]),
-        "lambda": trial.suggest_float("lambda", 1e-8, 1.0, log=True),
-        "alpha": trial.suggest_float("alpha", 1e-8, 1.0, log=True),
-    }
-    # 省略（独自のパラメータ設定のコード）
-
-    return param
-
-
-data = fetch_openml(name='Boston', version=1, as_frame=True)
-data_frame = data.frame
-data_frame["target"] = data.target
-
-run_framework(data_frame, "target", experiment_name="My_XGBoost_experiment",
-              param_tuning_func=custom_param_tuning, n_trials=5)
+if __name__ == '__main__':
+    unittest.main()
